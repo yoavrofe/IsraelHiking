5188b1f4f4ccb12ed945d49618ea3a61edba64cd
yoavrofe <yoav@amudanan.co.il>
2014-04-11 17:31:39 +0300

    Delete UploadTiles.bat

--- Site/UploadTiles.bat
+++
@@ -1,6 +0,0 @@
-SET ZIPFILE="%~nx1"
-cd %~dp0
-"%~dp0\..\..\WinSCP\WinSCP.com" /command "open ""Upload-osm.org.il""" "option reconnect 15" "option batch abort" "put -resume -preservetime -transfer=binary ""%ZIPFILE%""" "exit" 
-REM unzip
-"%~dp0\..\..\PuTTY\plink.exe" osm.org.il unzip -d ~/public_html/Israel\ Hiking/Tiles -o ~/temp/%ZIPFILE%
-"%~dp0\..\..\PuTTY\plink.exe" osm.org.il rm ~/temp/%ZIPFILE%
